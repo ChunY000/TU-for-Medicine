4782435d554acfea401b4c5da257e365608b9477
ChunY000 <102453838+ChunY000@users.noreply.github.com>
2022-04-13 08:28:22 +0800

    Update train.py

--- train.py
+++ train.py
@@ -57,8 +57,8 @@ if __name__ == "__main__":
     dataset_name = args.dataset
     dataset_config = {
         'Synapse': {
-            'root_path': '../data/Synapse/train_npz',
-            'list_dir': './lists/lists_Synapse',
+            'root_path': '/content/TransUNet/Data/Synapse/train_npz',
+            'list_dir': '/content/TransUNet/lists/lists_Synapse',
             'num_classes': 9,
         },
     }
